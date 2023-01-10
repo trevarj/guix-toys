5e63c4f343b2f7576c09b35a90933193e27761cd
jgart <jgart@dismail.de>
2023-01-09 23:29:49 -0600

    Add flat channel

--- channels.scm
+++ channels.scm
@@ -20,6 +20,14 @@
     (url "https://github.com/drewc/druix.git")
     (branch "main"))
   (channel
+    (name 'flat)
+    (url "https://github.com/flatwhatson/guix-channel.git")
+    (introduction
+      (make-channel-introduction
+       "33f86a4b48205c0dc19d7c036c85393f0766f806"
+        (openpgp-fingerprint
+         "736A C00E 1254 378B A982  7AF6 9DBE 8265 81B6 4490"))))
+  (channel
     (name 'guix-hpc)
     (url "https://gitlab.inria.fr/guix-hpc/guix-hpc.git"))
   (channel
