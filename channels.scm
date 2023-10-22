5e2bb2e53830c24aa51008a4e1341ffa61890fdc
jgart <jgart@dismail.de>
2023-10-21 20:25:18 -0500

    Add emacs-master channel

--- channels.scm
+++ channels.scm
@@ -3,6 +3,17 @@
 
 (list
   (toys-box
+    (forge "gitea")
+    (channel 
+      (channel
+        (name 'emacs-master)
+        (url "https://codeberg.org/akib/guix-channel-emacs-master")
+        (introduction
+         (make-channel-introduction
+          "1ba8c40e21c1c18f70c8ff116f2fbbbb41a5a30a"
+          (openpgp-fingerprint
+           "C954 CA9A BB4B EA43 417B  7151 5535 FCF5 4D88 616B"))))))
+  (toys-box
     (forge "sourcehut")
     (channel (channel
                 (name 'guixrus)
