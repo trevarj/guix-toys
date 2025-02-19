8f34929792ebf577cfa0d6df385520a54bc8fc43
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2025-02-19 13:45:51 -0300

    channels.scm: add selected-guix-works
    Signed-off-by: jgart <jgart@dismail.de>

--- channels.scm
+++ channels.scm
@@ -4,6 +4,18 @@
 (list
  (toys-box
   (forge "github")
+  (channel
+   (channel
+    (name 'selected-guix-works)
+    (url "https://github.com/gs-101/selected-guix-works")
+    (branch "main")
+    (introduction
+     (make-channel-introduction
+      "5d1270d51c64457d61cd46ec96e5599176f315a4"
+      (openpgp-fingerprint
+       "C780 21F7 34E4 07EB 9090  0CF1 4ACA 6D6F 89AB 3162"))))))
+ (toys-box
+  (forge "github")
   (synopsis "Channel for bleeding edge Emacs, with the commit updating
 every two hours by a GitHub workflow")
   (channel (channel
