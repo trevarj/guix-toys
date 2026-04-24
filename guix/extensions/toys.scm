63fb9faa53c575a42ea4440dd16c18b4339dad80
jgart <jgart@dismail.de>
2026-04-24 14:13:29 -0400

    Add marker for guix packaging phase

--- guix/extensions/toys.scm
+++ guix/extensions/toys.scm
@@ -19,6 +19,7 @@
 ;;; You should have received a copy of the GNU General Public License
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
+;;@load-paths@
 (define-module (guix extensions toys)
   #:use-module (toys client)
   #:use-module (toys cmd clone)
