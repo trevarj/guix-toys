7f48af1b376636ee6b438a11e7a2220365c3c9b7
Hilton Chain <hako@ultrarare.space>
2026-06-01 16:36:36 +0800

    channels: Add guix-gaming-games.

--- channels.scm
+++ channels.scm
@@ -336,6 +336,17 @@ Goblins and Hoot")
                      "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))))
 
   (toys-box
+    (forge "gitlab")
+    (channel (channel
+               (name 'guix-gaming-games)
+               (url "https://gitlab.com/guix-gaming-channels/games")
+               (introduction
+                (make-channel-introduction
+                 "c23d64f1b8cc086659f8781b27ab6c7314c5cca5"
+                 (openpgp-fingerprint
+                  "50F3 3E2E 5B0C 3D90 0424  ABE8 9BDC F497 A4BB CC7F"))))))
+
+  (toys-box
     (forge "sourcehut")
     (channel (channel
                (name 'trevdev)
